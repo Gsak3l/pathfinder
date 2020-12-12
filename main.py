5d4cd4867827fd3cd231ade0d254108faa504041
gsak3l <gsakel5@gmail.com>
2020-12-12 22:24:34 +0200

    colors added

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+# pip3 install pygame
+import pygame
+import math
+from queue import PriorityQueue
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+WIDTH = 800
+WIN = pygame.display.set_mode((WIDTH, WIDTH))  # square display window
+pygame.display.set_caption("A* Path Finding Algorithm")  # just the caption for the window
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# colors
+RED = (220, 20, 60)
+GREEN = (154, 205, 50)
+BLUE = (100, 149, 237)
+YELLOW = (255, 255, 51)
+WHITE = (255, 250, 250)
+BLACK = (0, 0, 0)
+PURPLE = (138, 43, 226)
+ORANGE = (255, 127, 80)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 209)
